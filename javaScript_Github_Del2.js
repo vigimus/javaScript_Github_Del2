ea0d87a0585a3f4837c10862af5d8e76e11cdac8
vigimus <160164175+vigimus@users.noreply.github.com>
2024-08-27 19:20:25 +0200

    Add files via upload

---
+++ javaScript_Github_Del2.js
@@ -0,0 +1,5 @@
+//Github man vill inte publicera saker så det förstör en sida hur som helst därmed behöver man använda bransches
+
+//Main bransch är hur allt ska se ut
+
+//git branch littlefeature
